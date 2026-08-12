647762152e6d4ea36b6b0fe2cfc5fa95dbbdbb19
Matheus-Dev-Java <matheus.dev.back23@gmail.com>
2026-08-11 23:42:41 -0300

    Organiza estrutura inicial do projeto em ddl, dml e dql

---
+++ dml/tipos_tabela.sql
@@ -0,0 +1,5 @@
+INSERT INTO tipos (nome_tipo) VALUES
+('Normal'), ('Fogo'), ('Água'), ('Elétrico'), ('Planta'),
+('Gelo'), ('Lutador'), ('Venenoso'), ('Terra'), ('Voador'),
+('Psíquico'), ('Inseto'), ('Pedra'), ('Fantasma'), ('Dragão'),
+('Sombrio'), ('Aço'), ('Fada');
