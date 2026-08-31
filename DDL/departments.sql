3cecd0c6ffbdde7cfefd9f8ae19bc58c0250914d
Matheus <matheus.dev.back23@gmail.com>
2026-08-31 14:16:01 -0300

    Format SQL table creation for departments

--- DDL/departments.sql
+++ DDL/departments.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE departments (
-    id        BIGINT       NOT NULL,
-    name      VARCHAR(255) NOT NULL,
-    branch_id BIGINT       NOT NULL   -- FK -> branches.id
+    id BIGINT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    branch_id BIGINT NOT NULL   -- FK -> branches.id
 );
 ALTER TABLE departments ADD PRIMARY KEY (id);
 CREATE INDEX idx_departments_branch_id ON departments (branch_id);
