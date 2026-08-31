2a377b428306c020f658ef4b97b136142ba2bef6
LuisFilipe <luisfilipeslima@gmail.com>
2026-08-30 23:52:10 -0300

    Delete dml/tipos_tabela.sql

--- dml/tipos_tabela.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tipos (nome_tipo) VALUES
-('Normal'), ('Fogo'), ('Água'), ('Elétrico'), ('Planta'),
-('Gelo'), ('Lutador'), ('Venenoso'), ('Terra'), ('Voador'),
-('Psíquico'), ('Inseto'), ('Pedra'), ('Fantasma'), ('Dragão'),
-('Sombrio'), ('Aço'), ('Fada');
