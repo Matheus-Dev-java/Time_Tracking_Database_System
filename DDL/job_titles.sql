be2204ab4e02a69619edba4373602670a52dcb18
Guilherme Barreto <contato.gbarreto7@gmail.com>
2026-09-01 21:25:13 -0300

    Delete DDL/job_titles.sql

--- DDL/job_titles.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE job_titles (
-    id BIGINT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    cbo VARCHAR(255) NOT NULL -- Brazilian Occupation Code
-);
-ALTER TABLE job_titles ADD PRIMARY KEY (id);
