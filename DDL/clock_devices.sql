e7e773a8544c45d952847957c0d65af7d821d20a
Guilherme Barreto <contato.gbarreto7@gmail.com>
2026-09-01 21:31:15 -0300

    Create clock_devices.sql

--- DDL/clock_devices.sql
+++ DDL/clock_devices.sql
@@ -1,19 +1,10 @@
 CREATE TABLE clock_devices (
-    id BIGINT  NOT NULL,
-    branch_id BIGINT NOT NULL, 
-    department_id BIGINT NOT NULL,   
-    location VARCHAR(255) NOT NULL,
-    model_name VARCHAR(255) NOT NULL   
+    id BIGINT NOT NULL,
+    branch_id BIGINT NOT NULL, -- FK -> branches.id
+    department_id BIGINT NOT NULL, -- FK -> departments.id
+    location VARCHAR(255) NOT NULL, -- Physical location description
+    model_name VARCHAR(255) NOT NULL -- Device model / app name
 );
-
 ALTER TABLE clock_devices ADD PRIMARY KEY (id);
 CREATE INDEX idx_clock_devices_branch_id ON clock_devices (branch_id);
 CREATE INDEX idx_clock_devices_department_id ON clock_devices (department_id);
-
-ALTER TABLE clock_devices
-    ADD CONSTRAINT fk_clock_devices_branch
-    FOREIGN KEY (branch_id) REFERENCES branches (id);
-
-ALTER TABLE clock_devices
-    ADD CONSTRAINT fk_clock_devices_department
-    FOREIGN KEY (department_id) REFERENCES departments (id);
