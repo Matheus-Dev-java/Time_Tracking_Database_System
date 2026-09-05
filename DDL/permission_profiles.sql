17674b1bdb6f888fb1af54d0820c30f487b8652c
BrendhaMariaDev <bredhaseixas@hotmail.com>
2026-09-05 12:01:03 -0300

    Delete DDL/permission_profiles.sql

--- DDL/permission_profiles.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE permission_profiles (
-    id BIGINT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    access_level INTEGER NOT NULL -- 1=Admin | 2=HR | 3=Manager | 4=Employee
-);
-ALTER TABLE permission_profiles ADD PRIMARY KEY (id);
