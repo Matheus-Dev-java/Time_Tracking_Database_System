fde7efaa982b3f8786f04ca188453266b5709064
BrendhaMariaDev <bredhaseixas@hotmail.com>
2026-09-05 12:01:26 -0300

    Delete DDL/ocurrences_types.sql

--- DDL/ocurrences_types.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE occurrence_types (
-    id BIGINT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    grants_hours BOOLEAN NOT NULL, -- TRUE = absence is paid / hours are not deducted
-    requires_doc BOOLEAN NOT NULL -- TRUE = employee must upload a proof document
-);
-ALTER TABLE occurrence_types ADD PRIMARY KEY (id);
