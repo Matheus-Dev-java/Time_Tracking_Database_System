c504509f6e9b2e24d2aff4761c41c1820274e822
Matheus <matheus.dev.back23@gmail.com>
2026-09-08 13:23:23 -0300

    Fix formatting of 'Noite' entry in SQL file

--- DML/fk_shifts_schedules.sql
+++ DML/fk_shifts_schedules.sql
@@ -27,7 +27,7 @@ INSERT INTO schedules (id, shift_name, workload, employee_id) VALUES
 (19, 'Manhã', 180, 19),
 (20, 'Tarde', 180, 20),
 (21, 'Comercial Integral', 220, 21),
-(22, 'Noite',                180, 22),
+(22, 'Noite', 180, 22),
 (23, 'Home Office Flex',   160, 23),
 (24, 'Comercial Integral', 220, 24),
 (25, 'Manhã',                180, 25),
